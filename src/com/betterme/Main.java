e2fe52ba8b36e1379a9d44b5c54552c9ac89f5be
ytian22 <ytian22@ncsu.edu>
2020-01-17 15:44:12 -0800

    Add problem 27 Remove elements

---
+++ src/com/betterme/Main.java
@@ -0,0 +1,12 @@
+package com.betterme;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RemoveElements27 re27 = new RemoveElements27();
+        int[] nums27 = new int[] {3, 2, 2, 3};
+        int result27 = re27.removeElement(nums27, 3);
+        System.out.println("Problem 27 RemoveElements: " + result27);
+    }
+
+}
