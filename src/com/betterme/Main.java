0f13a77538032552a2f96409005d9968bababdf2
ytian22 <ytian22@ncsu.edu>
2020-01-17 16:15:01 -0800

    Update problem27

--- src/com/betterme/Main.java
+++ src/com/betterme/Main.java
@@ -3,10 +3,11 @@ package com.betterme;
 public class Main {
 
     public static void main(String[] args) {
-        RemoveElements27 re27 = new RemoveElements27();
+        RemoveElement27 re27 = new RemoveElement27();
         int[] nums27 = new int[] {3, 2, 2, 3};
-        int result27 = re27.removeElement(nums27, 3);
-        System.out.println("Problem 27 RemoveElements: " + result27);
+        int result27_1 = re27.removeElement1(nums27, 3);
+        System.out.println("Problem 27 RemoveElements: " + result27_1);
+
     }
 
 }
---
+++ src/com/betterme/RemoveElement27.java
@@ -0,0 +1,32 @@
+package com.betterme;
+
+public class RemoveElement27 {
+    // Method 1 Two pointers, one traverses the array, the other updates the array if the element can be left.
+    public int removeElement1(int[] nums, int val) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j += 1;
+            }
+        }
+        return j;
+    }
+
+    // Method 2 Optimization, traverses the array, if find one to remove, swap with the last and tune the length.
+    // Note not to hurry in moving forward pointer, as the last element can also be removed.
+    public int removeElement2(int[] nums, int val) {
+        int i = 0;
+        int len = nums.length;
+        while (i < len) {
+            if (nums[i] == val) {
+                nums[i] = nums[len - 1];
+                len -= 1;
+            } else {
+                i += 1;
+            }
+        }
+        return len;
+    }
+
+}
